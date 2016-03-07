65ca79ff6f0f32bf5f3f9e76de58381d08a7a5a7
Pincheira Pablo <ppincheira@gisbertrepuestos.com.ar>
2016-03-07 16:55:39 -0300

    Elimine referencias

--- app/src/main/java/com/gisbert/it/pedidos/actividad/PedidoDetalleActivity.java
+++ app/src/main/java/com/gisbert/it/pedidos/actividad/PedidoDetalleActivity.java
@@ -8,8 +8,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
